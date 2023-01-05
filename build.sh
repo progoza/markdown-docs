9e6b7991798af82424f39dd7872e163fc2c569ad
Pawel Rogoza <pawel.rogoza@gmail.com>
2023-01-05 22:39:31 +0100

    ignore obsidian config

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 mkdir -p _output
 jekyll b -s ./cv -d ./_output/cv
 
+mkdir -p _build
+rm -rf ./_build/*
 mkdir -p _build/cs4kids
 cp -rf ./_theme/* _build/cs4kids/
 cp -rf ./cs4kids/* _build/cs4kids/
